a3dcfe35eac7a6d9a55d704b7a3133486d37c4e2
Ye Liu <liuye6y@gmail.com>
2017-11-24 11:03:28 +0800

    nav bar icon

--- src/components/bottomTabBar.js
+++ src/components/bottomTabBar.js
@@ -17,29 +17,18 @@ export default class BottomTabBar extends React.Component {
                 <TabBar.Item
                     title="首页"
                     key="Home"
-                    icon={<div style={{
-                        width: '22px',
-                        height: '22px',
-                        background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat' }}/>}
-                    selectedIcon={<div style={{
-                        width: '22px',
-                        height: '22px',
-                        background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat' }}/>}
+                    icon={<div className="home-normal-icon"/>}
+                    selectedIcon={<div className="home-active-icon"/>}
                     selected={this.props.selectedTab === 'home'}
                     onPress={ChangeRoute.goHomePage}
                 >
+
                 </TabBar.Item>
                 <TabBar.Item
                     title="项目"
                     key="Item"
-                    icon={<div style={{
-                        width: '22px',
-                        height: '22px',
-                        background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat' }}/>}
-                    selectedIcon={<div style={{
-                        width: '22px',
-                        height: '22px',
-                        background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat' }}/>}
+                    icon={<div className="item-normal-icon"/>}
+                    selectedIcon={<div className="item-active-icon"/>}
                     selected={this.props.selectedTab === 'purchaseItem'}
                     onPress={ChangeRoute.goPurchaseItemPage}
                 >
@@ -47,14 +36,8 @@ export default class BottomTabBar extends React.Component {
                 <TabBar.Item
                     title="企业"
                     key="Ent"
-                    icon={<div style={{
-                        width: '22px',
-                        height: '22px',
-                        background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat' }}/>}
-                    selectedIcon={<div style={{
-                        width: '22px',
-                        height: '22px',
-                        background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat' }}/>}
+                    icon={<div className="ent-normal-icon"/>}
+                    selectedIcon={<div className="ent-active-icon"/>}
                     selected={this.props.selectedTab === 'ent'}
                     onPress={ChangeRoute.goEntPage}
                 >
@@ -62,8 +45,8 @@ export default class BottomTabBar extends React.Component {
                 <TabBar.Item
                     title="统计"
                     key="statistic"
-                    icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
-                    selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+                    icon={<div className="static-normal-icon"/>}
+                    selectedIcon={<div className="static-active-icon"/>}
                     selected={this.props.selectedTab === 'statistic'}
                     onPress={ChangeRoute.goStaticPage}
                 >
